8ae053cac272cea3c6a45b4ebc0fd7b29f41195e
erik.levoll <erik.levoll@gmail.com>
2017-10-01 03:23:45 +0300

    added playback

--- script/script-min.js
+++ script/script-min.js
@@ -1,6 +1,6 @@
-function setVisible(e, i) {
-    i ? (e.removeClass("tab-hidden"), e.addClass("tab-visible")) : (e.removeClass("tab-visible"), 
-    e.addClass("tab-hidden"));
+function setVisible(i, t) {
+    t ? (i.removeClass("tab-hidden"), i.addClass("tab-visible")) : (i.removeClass("tab-visible"), 
+    i.addClass("tab-hidden"));
 }
 
 function setHomeView() {
@@ -11,6 +11,25 @@ function setContactView() {
     setVisible($(".home-main"), !1), setVisible($(".contact-main"), !0);
 }
 
+function playAudio(i, t) {
+    audioPlaying = t, t ? i.play() : i.pause();
+}
+
+function timeConvert(i) {
+    var t = "";
+    if (i < 60) t = "00:", i < 10 && (t += "0"), t += i; else {
+        var e = Math.floor(i / 60), n = i - 60 * e;
+        e < 10 && (t += "0"), t += e + ":", n < 10 && (t += "0"), t += n;
+    }
+    return t;
+}
+
+function timeToPercent(i, t) {
+    return i / t * 100;
+}
+
+var audioPlaying = !1, currentAudioSource;
+
 $(".home-btn").on("click", function() {
     setHomeView();
 }), $(".contact-btn").on("click", function() {
@@ -19,14 +38,28 @@ $(".home-btn").on("click", function() {
     setHomeView();
 }), $(".left-author.sub-author").on("click", function() {
     setHomeView();
-}), $(".player-main").find(".play-pause-wrapper").on("click", function() {
+}), $(".player-main").find(".audio-wrapper").on("click", function() {
+    var i = $(this).find("audio").get(0);
     $(this).hasClass("play-btn-playing") ? ($(this).removeClass("play-btn-playing"), 
-    $(this).addClass("play-btn-paused")) : ($(this).removeClass("play-btn-paused"), 
-    $(this).addClass("play-btn-playing"));
+    $(this).addClass("play-btn-paused"), playAudio(i, !1)) : ($(this).removeClass("play-btn-paused"), 
+    $(this).addClass("play-btn-playing"), playAudio(i, !0));
 }), $(document).ready(function() {
-    var e = window.location.href;
-    if (-1 != e.indexOf("#")) {
-        var i = e.split("/"), t = i[i.length - 1];
-        console.log(e), console.log(t);
+    var i = window.location.href;
+    if (-1 != i.indexOf("#")) {
+        var t = i.split("/"), e = t[t.length - 1];
+        console.log(i), console.log(e);
     }
+    $("audio").each(function() {
+        var i = $(this), t = i.get(0), e = i.parent(), n = e.siblings(".info-section"), a = e.siblings(".distance-indicator"), o = n.children(".info-time").find("p").children(), s = 0;
+        t.addEventListener("timeupdate", function() {
+            var i = t.currentTime, e = timeConvert(Math.round(i));
+            if (o.eq(0).html(e), s > 0) {
+                var n = timeToPercent(i, s);
+                a.css("width", n + "%");
+            }
+        }), t.addEventListener("loadedmetadata", function() {
+            var i = t.duration, e = timeConvert(Math.round(t.duration));
+            s = i, o.eq(2).html(e);
+        });
+    });
 });
--- script/script.js
+++ script/script.js
@@ -1,3 +1,6 @@
+var audioPlaying = false;
+var currentAudioSource;
+
 function setVisible(item, visible) {
   if (visible) {
     item.removeClass('tab-hidden');
@@ -35,27 +38,91 @@ $('.left-author.sub-author').on('click', function(){
   setHomeView();
 });
 
-$('.player-main').find('.play-pause-wrapper').on('click', function() {
+function playAudio(source, play) {
+  audioPlaying = play;
+  if (play) {
+    source.play();
+  } else {
+    source.pause();
+  }
+}
+
+$('.player-main').find('.audio-wrapper').on('click', function() {
+  var source = $(this).find('audio').get(0);
+  // var source = $(this > 'audio');
   if($(this).hasClass('play-btn-playing')) {
     $(this).removeClass('play-btn-playing');
     $(this).addClass('play-btn-paused');
+    playAudio(source, false);
   }
   else
   {
     $(this).removeClass('play-btn-paused');
     $(this).addClass('play-btn-playing');
+    playAudio(source, true);
   }
 });
+
+function timeConvert(inputTime) {
+  var convertedTime = "";
+  if (inputTime < 60) {
+    convertedTime = "00:";
+    if (inputTime < 10) { convertedTime += "0"; }
+    convertedTime += inputTime;
+  } else {
+    var minutes = Math.floor(inputTime / 60);
+    var seconds = (inputTime - minutes * 60);
+
+    if (minutes < 10) { convertedTime += "0"; }
+    convertedTime += minutes + ":";
+
+    if (seconds < 10) { convertedTime += "0" }
+    convertedTime += seconds;
+  }
+  return convertedTime
+}
+
+function timeToPercent(current, end) {
+  return current / end * 100;
+}
 
 $(document).ready(function(){
   var pathName = window.location.href;
 
-  if(pathName.indexOf('#') == -1) {
-     return;
+  if(pathName.indexOf('#') != -1) {
+    var split = pathName.split('/');
+    var last = split[split.length-1];
+    console.log(pathName);
+    console.log(last);
   }
 
-  var split = pathName.split('/');
-  var last = split[split.length-1];
-  console.log(pathName);
-  console.log(last);
+  $('audio').each(function() {
+    var sourceElement = $(this);
+    var source = sourceElement.get(0);
+    var parent = sourceElement.parent();
+    var info = parent.siblings('.info-section');
+    var distance = parent.siblings('.distance-indicator');
+    var infoTime = info.children('.info-time');
+    var time = infoTime.find('p').children();
+    var endDuration = 0.0;
+
+    source.addEventListener("timeupdate",function(){
+      var current = source.currentTime;
+      var roundTime = Math.round(current);
+      var convertedTime = timeConvert(roundTime);
+      time.eq(0).html(convertedTime);
+
+      if (endDuration > 0) {
+        var percent = timeToPercent(current, endDuration);
+        distance.css('width', percent + "%");
+      }
+    });
+
+    source.addEventListener('loadedmetadata', function() {
+      var dur = source.duration;
+      var endTime = timeConvert(Math.round(source.duration));
+      endDuration = dur;
+      time.eq(2).html(endTime);
+    });
+  });
 })
